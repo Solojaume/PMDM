83134492c088fb9ea3f767d47c91fbd457f2eac7
SoloJaume <jaumeponsorti@gmail.com>
2019-10-04 12:08:20 +0200

    Añadido Layouts

---
+++ AButons/app/src/main/java/com/example/abutons/Layout_Lineal.java
@@ -0,0 +1,14 @@
+package com.example.abutons;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Layout_Lineal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout__lineal);
+    }
+}
