afd0bd172126bda65f28908aa7b5b73978f7492c
soloJaume <jaumeponsorti@gmail.com>
2019-10-14 22:08:21 +0200

    EMpezado ejercicios layout

--- Ejercicios_Layout&&Widget/app/src/main/java/com/example/ejercicios_layoutwidget/MainActivity.java
+++ Ejercicios_Layout&&Widget/app/src/main/java/com/example/ejercicios_layoutwidget/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.ejercicios_layoutwidget;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         textview = findViewById(R.id.textView);
+    }
+    public void bacgroudToRed(View view){
+        textview.setBackgroundColor(R.color.ColorRed);
     }
 }
