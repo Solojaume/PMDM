5c442f5b44888b6879c64316dafbd20b86c901e6
SoloJaume <jaumeponsorti@gmail.com>
2019-10-30 09:40:48 +0100

    Añadido a Menu Basico un submenu

--- menu/app/src/main/java/com/example/menu/MainActivity.java
+++ menu/app/src/main/java/com/example/menu/MainActivity.java
@@ -26,16 +26,26 @@ public class MainActivity extends AppCompatActivity {
         String mensaje="";
         switch (item.getItemId()){
             case R.id.MnuOpc1:
-                Toast.makeText(getApplicationContext(), "Opcion 1 pulsada!", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Opcion 1 pulsada!", Toast.LENGTH_SHORT).show();
                 return true;
 
             case R.id.MnuOpc2:
-                Toast.makeText(getApplicationContext(), "Opcion 2 pulsada!", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Opcion 2 pulsada!", Toast.LENGTH_SHORT).show();
                 return true;
 
             case R.id.MnuOpc3:
-                Toast.makeText(getApplicationContext(), "Opcion 3 pulsada!", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Opcion 3 pulsada!", Toast.LENGTH_SHORT).show();
+                //Si ponemos Toast.LENGTH_Long no aparecera nunca el mensaje del menu
                 return true;
+
+            case R.id.SubMenu1:
+                Toast.makeText(getApplicationContext(), "Obcion 3.1 pulsada!", Toast.LENGTH_LONG).show();
+                return true;
+
+            case R.id.SubMenu2:
+                Toast.makeText(getApplicationContext(), "Obcion 3.2 pulsada!", Toast.LENGTH_LONG).show();
+                return true;
+
                 default:
                     return super.onOptionsItemSelected(item);
         }
