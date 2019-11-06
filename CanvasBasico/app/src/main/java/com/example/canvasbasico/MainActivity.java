9a7722285718392d23dc6060eb4b86790c04ef48
SoloJaume <jaumeponsorti@gmail.com>
2019-11-06 10:04:25 +0100

    Primer ejercicio de canvas resuelto en canvas basico

--- CanvasBasico/app/src/main/java/com/example/canvasbasico/MainActivity.java
+++ CanvasBasico/app/src/main/java/com/example/canvasbasico/MainActivity.java
@@ -25,14 +25,35 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onDraw(Canvas canvas) {
+//        protected void onDraw(Canvas canvas) {
+//            int height=getHeight()/2;
+//            int width= getWidth()/2;
+//            Paint pincel = new Paint();
+//            pincel.setColor(Color.RED);
+//            pincel.setStrokeWidth(15);
+//            pincel.setStyle(Paint.Style.FILL);
+//            canvas.drawCircle(width,height-75,80, pincel);
+//        }
+        protected  void onDraw(Canvas canvas){
             int height=getHeight()/2;
             int width= getWidth()/2;
             Paint pincel = new Paint();
-            pincel.setColor(Color.RED);
+            pincel.setColor(Color.MAGENTA);
             pincel.setStrokeWidth(15);
-            pincel.setStyle(Paint.Style.FILL);
-            canvas.drawCircle(width,height-75,80, pincel);
+            pincel.setStyle(Paint.Style.STROKE);
+            canvas.drawCircle(width,height,230, pincel);
+            pincel.setColor(Color.CYAN);
+            canvas.drawRect(width, height, width,height ,pincel);
+            pincel.setColor(Color.RED);
+            pincel.setStrokeWidth(80);
+            canvas.drawRect(width-450, 100, width-450,100 ,pincel);
+            pincel.setColor(Color.BLUE);
+            canvas.drawRect(width+450, 100, width+450, 100,pincel);
+            pincel.setColor(Color.GREEN);
+            canvas.drawRect(width-450, 1500, width-450, 1500,pincel);
+            pincel.setColor(Color.YELLOW);
+            canvas.drawRect(width+450, 1500, width+450, 1500,pincel);
+
         }
     }
 }
