02022e1e801636899674ea951ac7b7d7cd84f2f2
SoloJaume <jaumeponsorti@gmail.com>
2019-11-08 11:38:13 +0100

    Añadido seleccion sobre dos listas

---
+++ SeleccionSobreDosList/app/src/main/java/com/example/seleccionsobredoslist/Libro.java
@@ -0,0 +1,53 @@
+package com.example.seleccionsobredoslist;
+
+public class Libro {
+    private String nombre, autor, descripcion;
+    private int foto;
+
+    public Libro(String nombre, String autor, String descripcion) {
+        this.nombre = nombre;
+        this.autor = autor;
+        this.descripcion = descripcion;
+    }
+
+    public Libro(String nombre, String autor, String descripcion, int foto) {
+        this.nombre = nombre;
+        this.autor = autor;
+        this.descripcion = descripcion;
+        this.foto = foto;
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
---
+++ SeleccionSobreDosList/app/src/main/java/com/example/seleccionsobredoslist/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.seleccionsobredoslist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
+
+public class MainActivity extends AppCompatActivity {
+    Spinner sGenero ;
+    private String[] genero = {"Ciencia","Biologia","Teraplanismo"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         sGenero = (Spinner) findViewById(R.id.spinnerGenero);
+
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,genero);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sGenero.setAdapter(adapter);
+
+//        sGenero.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
+    }
+}
