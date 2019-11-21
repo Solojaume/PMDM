91cee086f19a215394b30eccd541c4d3692507ca
soloJaume <jaumeponsorti@gmail.com>
2019-11-21 22:25:15 +0100

    Añadido ejercicio repeticion del examen en proyecto EjercicioRepaso1Trimestre

---
+++ EjercicioRepaso1Trimestre2/app/src/main/java/com/example/ejerciciorepaso1trimestre/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.ejerciciorepaso1trimestre;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private static String seleccion;
+    ImageView imgResul;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.btn);
+        ImageView patinete = findViewById(R.id.img_11);
+        imgResul = findViewById(R.id.imgResul);
+        patinete.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                seleccion="electrico";
+                imgResul.setImageResource((R.drawable.patinete));
+            }
+
+        });
+
+        ImageView bicicleta = findViewById(R.id.img_12);
+        bicicleta.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                seleccion="bici";
+                imgResul.setImageResource((R.drawable.bicis));
+            }
+
+        });
+        ImageView coche = findViewById(R.id.img_13);
+        coche.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                seleccion="coche";
+                imgResul.setImageResource((R.drawable.coches));
+            }
+
+        });
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, Pantalla2.class);
+                Bundle myBundle=new Bundle();
+
+                myBundle.putString("ClaveTransporte",seleccion);
+                myIntent.putExtras(myBundle);
+                startActivity(myIntent);
+            }
+        });
+    }
+
+}
