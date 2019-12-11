772a806c6c9331208f615ec1964d043f7a9b06bf
SoloJaume <jaumeponsorti@gmail.com>
2019-12-11 10:44:00 +0100

    Añadidos  Fragmentos Button

--- FragmentoButton/app/src/main/java/com/example/fragmentobutton/MainActivity.java
+++ FragmentoButton/app/src/main/java/com/example/fragmentobutton/MainActivity.java
@@ -1,13 +1,58 @@
 package com.example.fragmentobutton;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    public Fragment mFrg;
+    int mStrackPosition=1;
+    Button manejaFragment;
+    int dibujos[]={R.drawable.ferrari,R.drawable.imagen};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        manejaFragment =findViewById(R.id.button);
+        manejaFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addFragment();
+            }
+        });
+        if(savedInstanceState==null){
+            Fragment miFragement=MiFragment.newInstance(dibujos[0]);
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.add(R.id.miFrg,miFragement);
+            ft.commit();
+        }
+        else {
+            mStrackPosition=savedInstanceState.getInt("position");
+        }
+
+
+    }
+
+    public void addFragment(){
+        Fragment miFragment;
+        Random r= new Random();
+        miFragment= MiFragment.newInstance(dibujos[r.nextInt(dibujos.length)]);
+        FragmentTransaction fg= getFragmentManager().beginTransaction();
+        fg.replace(R.id.miFrg,miFragment);
+        fg.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fg.addToBackStack(null);
+        fg.commit();
+    }
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistence){
+        super.onSaveInstanceState(outState,outPersistence);
+        outState. putInt("position",mStrackPosition);
     }
 }
--- FragmentoButton/app/src/main/java/com/example/fragmentobutton/MiFragment.java
+++ FragmentoButton/app/src/main/java/com/example/fragmentobutton/MiFragment.java
@@ -4,11 +4,12 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
+import android.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 /**
@@ -26,10 +27,8 @@ public class MiFragment extends Fragment {
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private int mParam1;
 
-    private OnFragmentInteractionListener mListener;
 
     public MiFragment() {
         // Required empty public constructor
@@ -44,11 +43,11 @@ public class MiFragment extends Fragment {
      * @return A new instance of fragment MiFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static MiFragment newInstance(String param1, String param2) {
+    public static MiFragment newInstance(int param1) {
         MiFragment fragment = new MiFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putInt(ARG_PARAM1, param1);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -57,8 +56,7 @@ public class MiFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mParam1 = getArguments().getInt(ARG_PARAM1);
         }
     }
 
@@ -66,45 +64,12 @@ public class MiFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mi, container, false);
+        View v=inflater.inflate(R.layout.fragment_mi, container, false);
+        ImageView mifoto=(ImageView) v.findViewById(R.id.imagen);
+        mifoto.setImageResource(mParam1);
+        return v;
+
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
