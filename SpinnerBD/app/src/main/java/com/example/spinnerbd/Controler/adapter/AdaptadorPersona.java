1e9437ca6780bd4fe75e9b8d86a5b553f98c5bea
SoloJaume <jaumeponsorti@gmail.com>
2020-01-10 10:44:49 +0100

    Perfilado adaptador persona de spinerbd

--- SpinnerBD/app/src/main/java/com/example/spinnerbd/Controler/adapter/AdaptadorPersona.java
+++ SpinnerBD/app/src/main/java/com/example/spinnerbd/Controler/adapter/AdaptadorPersona.java
@@ -1,9 +1,11 @@
 package com.example.spinnerbd.Controler.adapter;
 
 import android.content.Context;
+import android.view.View;
 import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.example.spinnerbd.MainActivity;
 import com.example.spinnerbd.R;
@@ -12,8 +14,24 @@ import com.example.spinnerbd.model.Persona;
 import java.util.List;
 
 public class AdaptadorPersona  extends ArrayAdapter<Persona> {
-
+    private
     public AdaptadorPersona(@NonNull Context context, @NonNull List<Persona> objects) {
         super(context, R.layout.item_persona,objects);
+        this.context = context;
+        this.personas = objects;
+    }
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View v = convertView;
+        if(v == null)
+            v = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+
+        Persona p = personas[position];
+        TextView tvName = v.findViewById(R.id.tv_person_name);
+        tvName.setText(p.getName() + " (" + p.getAge() + " años)");
+
+
+        return v;
     }
 }
