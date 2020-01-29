82e00609ba627f8a74aec9851ff6b0098deeaac1
SoloJaume <jaumeponsorti@gmail.com>
2020-01-29 11:21:55 +0100

    Corregido ProyectoFinal

---
+++ ProyectoFInal/app/src/main/java/com/example/proyectofinal/controller/GeneralConf.java
@@ -0,0 +1,21 @@
+package com.example.proyectofinal.controller;
+
+public class GeneralConf {
+    private static final String DATABASE_NAME = "DBFinal";
+    private static final int DATABASE_VERSION = 3;
+
+    public static String getDatabaseName() {
+        return DATABASE_NAME;
+    }
+
+    public static int getDatabaseVersion() {
+        return DATABASE_VERSION;
+    }
+}
+class user{
+    private static final String ID ="id";
+    private static final String USER = "user";
+    private static final String PASSWORD = "password";
+
+
+}
