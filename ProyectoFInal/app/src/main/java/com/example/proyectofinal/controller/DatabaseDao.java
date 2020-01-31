a0bd34d97d6b5027f57f20774dd56712a38625dd
SoloJaume <jaumeponsorti@gmail.com>
2020-01-31 12:08:32 +0100

    Añadido GeneralCof

---
+++ ProyectoFInal/app/src/main/java/com/example/proyectofinal/controller/DatabaseDao.java
@@ -0,0 +1,6 @@
+package com.example.proyectofinal.controller;
+
+
+
+public class DatabaseDao {
+}
--- ProyectoFInal/app/src/main/java/com/example/proyectofinal/controller/GeneralConf.java
+++ ProyectoFInal/app/src/main/java/com/example/proyectofinal/controller/GeneralConf.java
@@ -1,21 +1,32 @@
 package com.example.proyectofinal.controller;
 
 public class GeneralConf {
-    private static final String DATABASE_NAME = "DBFinal";
-    private static final int DATABASE_VERSION = 3;
+    protected static final String DATABASE_NAME = "DBFinal";
+    protected static final int DATABASE_VERSION = 3;
 
-    public static String getDatabaseName() {
-        return DATABASE_NAME;
-    }
+    //Tabla Usuario
+    protected static final String DATABASE_TABLE_USER="user";
+    protected static final String U_ID ="id";
+    protected static final String USERNAME = "username";
+    protected static final String U_PASSWORD = "password";
+    protected static final String U_INICIADO = "iniciado";
+    protected static final String[] l={DATABASE_TABLE_USER,U_ID,USERNAME,U_PASSWORD,U_INICIADO};
+    protected static final String DATABASE_CREATE_USER=
+            "create table "+DATABASE_TABLE_USER+" ("+U_ID+ " integer primary key,"+USERNAME+" text not null, "
+            +U_PASSWORD+" text not null, "+U_INICIADO+ " integer not null)";
 
-    public static int getDatabaseVersion() {
-        return DATABASE_VERSION;
-    }
+     //Tabla Preferencia usuario
+    protected static final String DATABASE_TABLE_PREFERENCIAS="preferencias";
+    protected static final String PREFENCIAS_ID ="id";
+    protected static final String P_USER_ID = "user_id";
+    protected static final String P_GENERO_ID = "genero_id";
+
+
+    //Tabla Genero
+    protected static final String DATABASE_TABLE_GENEROS="genero";
+    protected static final String GENERO_ID ="id";
+    protected static final String G_NAME  = "name";
+
+
 }
-class user{
-    private static final String ID ="id";
-    private static final String USER = "user";
-    private static final String PASSWORD = "password";
 
-
-}
